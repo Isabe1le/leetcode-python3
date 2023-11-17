ebc0cd7619f6c11cfcf235acd39b5e0d16b0112f
Isabelle <meguminperson@gmail.com>
2023-11-17 12:52:44 +0000

    refactor: 41ms -> 28ms runtime

--- merge_strings_alternately.py
+++ merge_strings_alternately.py
@@ -2,14 +2,20 @@
 
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        min_word_len = min(len(word1), len(word2))
+        word1_len = len(word1)
+        word2_len = len(word2)
+        min_word_len = min(word1_len, word2_len)
         merged_word  = ""
 
         # Merge the words alternately
-        for i in range(min_word_len):
+        # While-loops are faster than for-loops for
+        # small integers as of 3.11
+        i = 0
+        while i < min_word_len:
             word1_c = word1[i]
             word2_c = word2[i]
-            merged_word += f"{word1_c}{word2_c}"
+            merged_word += word1_c + word2_c
+            i += 1
 
         # Add on any excess chars
         merged_word += word1[min_word_len::]
